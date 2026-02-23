74c367ba9465f5595883a5073e0da4270efcbda5
akshaygill01 <gillakshay051@gmail.com>
2026-02-24 02:06:17 +0530

    added eureka service registry

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class ActivityserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ActivityserviceApplication.class, args);
 	}
-
 }
